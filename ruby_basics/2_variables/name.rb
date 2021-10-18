e8181b8e2256a870b1b6eef54f81f0b54a892d9d
gloe2019 <gloedem@gmail.com>
2021-10-18 12:32:27 -0400

    added new folder with launch school variable exercises

---
+++ ruby_basics/2_variables/name.rb
@@ -0,0 +1,9 @@
+print 'Please enter your first name: '
+first_name = gets.chomp
+print 'Please enter your last name: '
+last_name = gets.chomp
+puts "#{last_name}, #{first_name}"
+
+# 10.times do
+#   puts "#{first_name}!"
+# end
